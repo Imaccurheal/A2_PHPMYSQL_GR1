ce9c1ca6d5fcbafa752cc47e83cc9e0ffa20dafb
BenjaminLR <benjamin.le.roux.lfp@gmail.com>
2014-10-26 20:52:21 +0100

    updating the back office crud articles

--- admin-article-add.php
+++ admin-article-add.php
@@ -31,6 +31,36 @@ if (!empty($_POST) && isset($_POST['submitArticle'])) {
     }
 }
 
-include __DIR__.'/template/admin-article-add.php';
+$categories = getCategories($link);
+$tags = getTags($link);
+
+isConnected() ? $username = $_SESSION['username'] : $username = null;
+
+echo $twig->render('admin-article-add.html.twig', [
+    // direction links in the header
+    'home'                  => ROOT,
+    'categories'            => ROOT.'categories.php',
+    'tags'                  => ROOT.'tags.php',
+    'contact'               => ROOT.'contact.php',
+    'admin-article-list'    => ROOT.'admin-article-list.php',
+    'admin-article-add'     => ROOT.'admin-article-add.php',
+    'admin-category-list'   => ROOT.'admin-category-list.php',
+    'admin-category-add'    => ROOT.'admin-category-add.php',
+    'admin-tag-list'        => ROOT.'admin-tag-list.php',
+    'admin-tag-add'         => ROOT.'admin-tag-add.php',
+    'admin-user-list'       => ROOT.'admin-user-list.php',
+    'admin-user-add'        => ROOT.'admin-user-add.php',
+    'login'                 => ROOT.'login.php',
+    'logout'                => ROOT.'logout.php',
+    //
+    //'mandatory' => $mandatory,
+    'errors'                => $errors,
+    'var_categories'        => $categories,
+    'var_tags'              => $tags,
+    // checking status connection
+    'connected'             => isConnected(),
+    'role'                  => isAdmin(),
+    'username'              => $username,
+    ]);
 
 require __DIR__.'/_footer.php';
